a229b12fd9ec8b6c3059dc877f766875e29576a1
snork2 <strange.snork@gmail.com>
2022-02-26 22:49:58 +0300

    Интеграция с обновленным CCSDS

--- src/rpi/ccsds-link/server-uslp/src/main.cpp
+++ src/rpi/ccsds-link/server-uslp/src/main.cpp
@@ -11,13 +11,18 @@
 #include <ccsds/uslp/map/map_packet_emitter.hpp>
 #include <ccsds/uslp/map/map_access_emitter.hpp>
 
+#include <ccsds/uslp/physical/mchannel_demuxer.hpp>
+#include <ccsds/uslp/master/vchannel_demuxer.hpp>
+#include <ccsds/uslp/virtual/map_demuxer.hpp>
+#include <ccsds/uslp/map/map_packet_acceptor.hpp>
+#include <ccsds/uslp/map/map_access_acceptor.hpp>
+
+
 #include "json.hpp"
 
 
 #define RADIO_FRAME_SIZE (200)
 
-
-ccsds::uslp::input_stack istack;
 
 class ostack
 {
@@ -27,7 +32,7 @@ public:
 		using namespace ccsds;
 		using namespace ccsds::uslp;
 
-		auto phys = _stack.create_pchannel<mchannel_rr_muxer>("lora");
+		mchannel_rr_muxer * phys = _stack.create_pchannel<mchannel_rr_muxer>("lora");
 		phys->frame_size(RADIO_FRAME_SIZE);
 		phys->error_control_len(error_control_len_t::ZERO);
 
@@ -37,8 +42,10 @@ public:
 		auto virt = _stack.create_vchannel<map_rr_muxer>(gvcid_t(master->channel_id, 0x00));
 		virt->frame_seq_no_len(2);
 
-		_command_channel = _stack.create_map<map_access_emitter>(gmapid_t(virt->channel_id, 0x00));
-		_ip_channel = _stack.create_map<map_packet_emitter>(gmapid_t(virt->channel_id, 0x01));
+		_ip_channel = _stack.create_map<map_packet_emitter>(gmapid_t(virt->channel_id, 0x00));
+		_command_channel = _stack.create_map<map_access_emitter>(gmapid_t(virt->channel_id, 0x01));
+
+		phys->finalize();
 	}
 
 
@@ -55,6 +62,38 @@ private:
 	ccsds::uslp::map_access_emitter * _command_channel;
 	ccsds::uslp::output_stack _stack;
 
+};
+
+
+class istack
+{
+public:
+	istack()
+	{
+		using namespace ccsds;
+		using namespace ccsds::uslp;
+
+		mchannel_demuxer * phys;
+		phys = _stack.create_pchannel<mchannel_demuxer>("lora");
+		phys->insert_zone_size(0);
+		phys->error_control_len(error_control_len_t::ZERO);
+
+		vchannel_demuxer * master;
+		master = _stack.create_mchannel<vchannel_demuxer>(mcid_t(0x42));
+
+		map_demuxer * virt;
+		virt = _stack.create_vchannel<map_demuxer>(gvcid_t(master->channel_id, 0x00));
+
+		_ip_channel = _stack.create_map<map_packet_acceptor>(gmapid_t(virt->channel_id, 0x00));
+		_telemetry_channel = _stack.create_map<map_packet_acceptor>(gmapid_t(virt->channel_id, 0x01));
+
+		phys->finalize();
+	}
+
+private:
+	ccsds::uslp::map_packet_acceptor * _ip_channel;
+	ccsds::uslp::map_packet_acceptor * _telemetry_channel;
+	ccsds::uslp::input_stack _stack;
 };
 
 
