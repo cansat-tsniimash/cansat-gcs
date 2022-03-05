b97a95bd1cce7b2e728f5d630e8ec5c8a0652ecc
snork2 <strange.snork@gmail.com>
2022-03-06 00:29:57 +0300

    генерация debug и release проектов

--- src/rpi/ccsds-link-gen-eclipse.sh
+++ src/rpi/ccsds-link-gen-eclipse.sh
@@ -3,4 +3,5 @@
 THIS_DIR=$(dirname ${0})
 
 mkdir -p ${THIS_DIR}/ccsds-link-build/
-cmake -G "Eclipse CDT4 - Ninja" -S ${THIS_DIR}/ccsds-link -B ${THIS_DIR}/ccsds-link-build
+cmake -G "Eclipse CDT4 - Ninja" -S ${THIS_DIR}/ccsds-link -B ${THIS_DIR}/ccsds-link-build/debug -DCMAKE_BUILD_TYPE=Debug
+cmake -G "Eclipse CDT4 - Ninja" -S ${THIS_DIR}/ccsds-link -B ${THIS_DIR}/ccsds-link-build/release -DCMAKE_BUILD_TYPE=Release
