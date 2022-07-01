45e716bfd2fc7f9bbbb4af8e9d20ab0012e44f71
snork2 <vasilly.prokopyev@gmail.com>
2022-07-01 17:53:36 +0300

    Лампочка мигает лучше

--- src/rpi/tele_blink.py
+++ src/rpi/tele_blink.py
@@ -18,17 +18,27 @@ def main():
 
     sub_socket.connect(sub_ep)
     sub_socket.setsockopt(zmq.SUBSCRIBE, MAVLINK_DATA_TOPIC)
+    poller = zmq.Poller()
+    poller.register(sub_socket, zmq.POLLIN)
 
     chip = gpiod.Chip(LED_CHIP)
     led_lines = chip.get_lines([LED_LINE])
     led_lines.request(consumer="its_tele_blink", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
 
+    # while True:
+    #     led_lines.set_values([1])
+    #     time.sleep(0.5)
+    #     led_lines.set_values([0])
+    #     time.sleep(1)
+
     while True:
-        msgs = sub_socket.recv_multipart()
-        print("got msgs")
-        led_lines.set_values([0])
-        time.sleep(0.2)
-        led_lines.set_values([1])
+        events = dict(poller.poll(int(0.5*1000)))
+        if sub_socket in events:
+            msgs = sub_socket.recv_multipart()
+            print("got msgs %s", msgs)
+            led_lines.set_values([1])
+        else:
+            ed_lines.set_values([0])
 
     del ctx
     return 0
