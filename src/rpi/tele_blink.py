bbef800db9b1b2d5d25a0135ed4ff0f464361ce6
snork2 <vasilly.prokopyev@gmail.com>
2022-07-02 15:16:58 +0300

    Добавлена лампочка для FIX на ручную наземку

--- src/rpi/tele_blink.py
+++ src/rpi/tele_blink.py
@@ -3,10 +3,13 @@ import zmq
 import time
 import os
 
+from pymavlink.dialects.v20.its import MAVLink, MAVLink_bad_data, MAVLink_gps_ubx_nav_sol_message
+
 
 MAVLINK_DATA_TOPIC = b"radio.downlink_frame"
 LED_CHIP = "/dev/gpiochip0"
-LED_LINE = 21
+TM_LED_LINE = 21
+FIX_LED_LINE = 20
 
 
 def main():
@@ -22,23 +25,31 @@ def main():
     poller.register(sub_socket, zmq.POLLIN)
 
     chip = gpiod.Chip(LED_CHIP)
-    led_lines = chip.get_lines([LED_LINE])
-    led_lines.request(consumer="its_tele_blink", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0])
+    tm_led_lines = chip.get_lines([TM_LED_LINE, FIX_LED_LINE])
+    tm_led_lines.request(consumer="its_tele_blink", type=gpiod.LINE_REQ_DIR_OUT, default_vals=[0, 0])
 
-    # while True:
-    #     led_lines.set_values([1])
-    #     time.sleep(0.5)
-    #     led_lines.set_values([0])
-    #     time.sleep(1)
+    mav = MAVLink(file=None)
+    mav.robust_parsing = True
 
     while True:
+        tm_led_value = 0
+        fix_led_value = 0
+
         events = dict(poller.poll(int(0.5*1000)))
         if sub_socket in events:
             msgs = sub_socket.recv_multipart()
+            tm_led_value = 1
             print("got msgs %s", msgs)
-            led_lines.set_values([1])
-        else:
-            led_lines.set_values([0])
+
+            mavlink_payload = msgs[2]
+            packets = mav.parse_buffer(mavlink_payload)
+            for p in packets or []:
+                if isinstance(p, MAVLink_gps_ubx_nav_sol_message):
+                    fix = p.gpsFix
+                    if fix != 0:
+                        fix_led_value = 1
+
+        led_lines.set_values([tm_led_value, fix_led_value])
 
     del ctx
     return 0
