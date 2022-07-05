04ede518822c6a42fddb30fddb7d3e0eefc77ee9
snork2 <vasilly.prokopyev@gmail.com>
2022-07-05 12:43:20 +0300

    make_csv обновлен под новые задачи, которые ставит перед нами новая телеметрия

--- src/zmq/make_csv.py
+++ src/zmq/make_csv.py
@@ -1,8 +1,10 @@
+import glob
 import os
 import argparse
 import sys
 import csv
 import collections
+import sys
 from datetime import datetime
 from wgs84 import wgs84_xyz_to_latlonh
 
@@ -44,6 +46,13 @@ class MsgProcessor:
         self.writer = None
         self.message_count = 0
         self.notimestamps = notimestamps
+        self.time_steady_range = (None, None) # type: typing.Tuple[typing.Optiona[int], typing.Optiona[int]]
+        self.time_steady_launch = None
+        self.time_launch = None
+        # для фикса бага с переполнением time_steady БКУ
+        self.fix_bcu_electrical_ts = False
+        self.bcu_electrical_time_steady_offsets = collections.defaultdict(lambda: 0)
+        self.bcu_electrical_time_steady_prevs = collections.defaultdict(lambda: 0)
 
     def accept(self, msg):
         self.msg_id = msg.get_msgId()
@@ -75,6 +84,12 @@ class MsgProcessor:
                     "time_gregorian": "<invalid>"
                 })
 
+            if self.time_launch is not None:
+                time_from_launch = msg_dict["time_s"] - self.time_launch
+                mins_from_launch = (time_from_launch + msg_dict["time_us"] / (1000 * 1000)) / 60
+                msg_dict["time_s_from_launch"] = time_from_launch
+                msg_dict["mins_from_launch"] = mins_from_launch
+
         # Добавляем таймштамп из mavlog файла
         if not self.notimestamps:
             ts = msg._timestamp
@@ -95,6 +110,43 @@ class MsgProcessor:
                 "h": h,
             })
 
+        # time_steady в БКУ переполняется после 0xFFFFFFFF
+        # Исправим это
+        # К сожалению это все будет очень плохо выглядеть в случае, если были
+        # ребуты. Но у нас их не было в этот раз, поэтому покатит
+        if self.fix_bcu_electrical_ts and "ELECTRICAL_STATE" == self.msg_class.name:
+            if hdr.srcSystem == 10: # 10 == БКУ
+                component = hdr.srcComponent
+                prev = self.bcu_electrical_time_steady_prevs[component]
+                current = msg_dict["time_steady"]
+                corrected = current + self.bcu_electrical_time_steady_offsets[component]
+                if corrected < prev:
+                    # в исходниках бку было (uint32_t)now / 1000
+                    # То есть переполнение случается со смещением на 3 порядка
+                    correction = int(0xFFFFFFFF / 1000)
+                    corrected += correction
+                    self.bcu_electrical_time_steady_offsets[component] += correction
+
+                self.bcu_electrical_time_steady_prevs[component] = corrected
+                msg_dict["time_steady"] = corrected
+
+        if "time_steady" in msg_dict:
+            left = self.time_steady_range[0]
+            right = self.time_steady_range[1]
+            that = msg_dict["time_steady"]
+            if left is not None and that < left:
+                return
+
+            if right is not None and that > right:
+                return
+
+            if self.time_steady_launch is not None:
+                time_steady = msg_dict["time_steady"]
+                time_steady_from_launch = time_steady - self.time_steady_launch
+                mins_steady_from_launch = time_steady_from_launch / 1000 / 60
+                msg_dict["time_steady_from_launch"] = time_steady_from_launch
+                msg_dict["mins_steady_from_launch"] = mins_steady_from_launch
+
         if "mavpackettype" in msg_dict:
             del msg_dict["mavpackettype"]  # Совершенно излишне в нашем случае
 
@@ -103,8 +155,23 @@ class MsgProcessor:
             fstem = "%s-%s-%s" % (self.msg_class.name, msg_hdr.srcSystem, msg_hdr.srcComponent)
             fpath = os.path.join(self.base_path, fstem + ".csv")
 
+            # Сортиранем время в начало таблички
+            fieldnames = list(msg_dict.keys())
+
+            def pull_front(column):
+                if column in fieldnames:
+                    fieldnames.remove(column)
+                    fieldnames.insert(0, column)
+
+            # pull_front("mins_steady_from_launch")
+            # pull_front("time_steady_from_launch")
+            # pull_front("time_steady")
+            # pull_front("time_gregorian")
+            # pull_front("time_us")
+            # pull_front("time_s")
+
             self.stream = open(fpath, mode="w", newline='')
-            self.writer = csv.DictWriter(self.stream, fieldnames=msg_dict.keys())
+            self.writer = csv.DictWriter(self.stream, fieldnames=fieldnames)
             self.writer.writeheader()
 
         print(msg)  # Напечатаем сообщение для наглядности
@@ -114,50 +181,67 @@ class MsgProcessor:
 
 def main(argv):
     parser = argparse.ArgumentParser("tm parser to csvs", add_help=True)
-    parser.add_argument("-i,--input", nargs="?", dest="input", required=True)
-    parser.add_argument("-o,--output_dir", nargs="?", dest="output_dir", help="If output is None, input filename is name for output directory", default=None)
+    parser.add_argument("--input", "-i", nargs="?", dest="input", required=True)
+    parser.add_argument("--output-dir", "-o", nargs="?", dest="output_dir", help="If output is None, input filename is name for output directory", default=None)
     parser.add_argument("--notimestamps", dest="notimestamps", default=False, action='store_true')
+    parser.add_argument("--time-steady-start", nargs="?", type=int, dest="time_steady_start", default=None)
+    parser.add_argument("--time-steady-stop", nargs="?", type=int, dest="time_steady_stop", default=None)
+    parser.add_argument("--time-steady-launch", nargs="?", type=int, dest="time_steady_launch", default=None)
+    parser.add_argument("--time-launch", nargs="?", type=int, dest="time_launch", default=None)
+    parser.add_argument("--fix-bcu-electricals", action="store_true", dest="fix_bcu_electricals")
     args = parser.parse_args(argv)
 
-    base_path = args.output_dir
-    f = args.input
+    base_path_ = args.output_dir
+    files = glob.iglob(args.input)
     notimestamps = args.notimestamps
+    time_steady_range = (args.time_steady_start, args.time_steady_stop)
+    time_steady_launch = args.time_steady_launch
+    time_launch = args.time_launch
+    fix_bcu_electricals = args.fix_bcu_electricals
 
-    if not base_path:
-        fname = os.path.split(f)[1]
-        base_path = os.path.splitext(fname)[0] + "_mav_csv"
+    for f in files:
+        base_path = base_path_
+        if not base_path:
+            base_path = os.path.splitext(f)[0] + "_mav_csv"
 
-    if not os.path.isdir(base_path):
-        os.makedirs(base_path, exist_ok=True)
+        if not os.path.isdir(base_path):
+            os.makedirs(base_path, exist_ok=True)
 
-    con = mavutil.mavlogfile(f, notimestamps=notimestamps)
-    processors = {}
-    bad_data_bytes = 0
+        con = mavutil.mavlogfile(f, notimestamps=notimestamps)
+        processors = {}
+        bad_data_bytes = 0
 
-    while True:
-        msg = con.recv_msg()
-        if not msg:
-            break
+        while True:
+            msg = con.recv_msg()
+            if not msg:
+                break
 
-        msg_id = msg.get_msgId()
-        if msg_id < 0:
-            bad_data_bytes += 1
-            continue
+            msg_id = msg.get_msgId()
+            if msg_id < 0:
+                bad_data_bytes += 1
+                continue
 
-        msg_hdr = msg.get_header()
-        msg_key = "%s-%s-%s" % (msg_id, msg_hdr.srcSystem, msg_hdr.srcComponent)
-        if msg_key not in processors:
-            processor = MsgProcessor(base_path, notimestamps=notimestamps)
-            processors.update({msg_key: processor})
+            msg_hdr = msg.get_header()
+            msg_key = "%s-%s-%s" % (msg_id, msg_hdr.srcSystem, msg_hdr.srcComponent)
+            if msg_key not in processors:
+                processor = MsgProcessor(base_path, notimestamps=notimestamps)
+                processor.time_steady_range = time_steady_range
+                processor.time_steady_launch = time_steady_launch
+                processor.fix_bcu_electrical_ts = fix_bcu_electricals
+                processor.time_launch = time_launch
+                processors.update({msg_key: processor})
 
-        processor = processors[msg_key]
-        processor.accept(msg)
+            processor = processors[msg_key]
+            processor.accept(msg)
 
-    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-    print("Stats in total:")
-    for msg_key, processor in processors.items():
-        print("%s-%s: %d messages" % (processor.msg_class.name, msg_key, processor.message_count))
-    print("bad data %s bytes in total" % bad_data_bytes)
+        print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+        print("Stats in total for file %s:" % f, file=sys.stderr)
+        for msg_key, processor in processors.items():
+            print(
+                "%s-%s: %d messages" % (processor.msg_class.name, msg_key, processor.message_count),
+                file=sys.stderr
+            )
+        print("bad data %s bytes in total" % bad_data_bytes, file=sys.stderr)
 
 
 if __name__ == "__main__":
